3b26fc44735f23046ecd2026738e97d5aaf038b1
chen <2499629551@qq.com>
2018-11-01 09:00:43 +0800

    添加了登录界面

--- src/main/java/com/ssm/controller/loginController.java
+++ src/main/java/com/ssm/controller/loginController.java
@@ -26,7 +26,7 @@ public class loginController {
             if (usertype==0) return "redirect:/admin" ;//重定向登陆成功界面,重定向到管理员页面
             if (usertype==1) return "redirect:/register" ;//重定向登陆成功界面,重定向到管理员页面
         else {
-            model.addAttribute();
+            model.addAttribute("error","登录失败！");
             return "login";
             }
     }
